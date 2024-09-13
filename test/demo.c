e1611f7bbb697f97b42e57b5cbf28fdc1697204e
Niklas Hörnblad <c19nhd@cs.umu.se>
2024-09-13 16:09:36 +0200

    Removed invalid demo

--- test/demo.c
+++ test/demo.c
@@ -10,7 +10,6 @@
 #include <complex.h>
 
 void contraction();
-void contraction2();
 void hadamard();
 void complex_num();
 void conjugate();
@@ -28,8 +27,6 @@ int main(int argc, char const *argv[])
 {
     printf("Contraction: \n");
     contraction();
-    printf("Contraction 2: \n");
-    contraction2();
     printf("Hadamard: \n");
     hadamard();
     printf("Complex: \n");
@@ -153,121 +150,6 @@ void contraction() {
     TAPP_execute_product(plan, exec, &status, (void*)&alpha, (void*)A, (void*)B, (void*)&beta, (void*)C, (void*)D);
 
     print_tensor_s(nmode_D, extents_D, strides_D, D);
-
-    TAPP_destory_tensor_product(plan);
-    TAPP_destory_tensor_info(info_A);
-    TAPP_destory_tensor_info(info_B);
-    TAPP_destory_tensor_info(info_C);
-    TAPP_destory_tensor_info(info_D);
-}
-
-void contraction2() {
-    int nmode_A = 3;
-    int64_t extents_A[3] = {4, 3, 3};
-    int64_t strides_A[3] = {1, 4, 12};
-    TAPP_tensor_info info_A;
-    TAPP_create_tensor_info(&info_A, TAPP_F32, nmode_A, extents_A, strides_A);
-
-    int nmode_B = 4;
-    int64_t extents_B[4] = {3, 2, 2, 3};
-    int64_t strides_B[4] = {1, 3, 6, 12};
-    TAPP_tensor_info info_B;
-    TAPP_create_tensor_info(&info_B, TAPP_F32, nmode_B, extents_B, strides_B);
-
-    int nmode_C = 3;
-    int64_t extents_C[3] = {4, 2, 2};
-    int64_t strides_C[3] = {1, 4, 8};
-    TAPP_tensor_info info_C;
-    TAPP_create_tensor_info(&info_C, TAPP_F32, nmode_C, extents_C, strides_C);
-    
-    int nmode_D = 3;
-    int64_t extents_D[3] = {2, 4, 2};
-    int64_t strides_D[3] = {1, 4, 16};
-    TAPP_tensor_info info_D;
-    TAPP_create_tensor_info(&info_D, TAPP_F32, nmode_D, extents_D, strides_D);
-
-    TAPP_handle handle;
-    TAPP_tensor_product plan;
-    TAPP_element_op op_A = TAPP_IDENTITY;
-    TAPP_element_op op_B = TAPP_IDENTITY;
-    TAPP_element_op op_C = TAPP_IDENTITY;
-    TAPP_element_op op_D = TAPP_IDENTITY;
-    int64_t idx_A[3] = {'a', 'b', 'c'};
-    int64_t idx_B[4] = {'c', 'd', 'e', 'b'};
-    int64_t idx_C[3] = {'a', 'd', 'e'};
-    int64_t idx_D[3] = {'e', 'a', 'd'};
-    TAPP_prectype prec = TAPP_DEFAULT_PREC;
-    TAPP_create_tensor_product(&plan, handle, op_A, info_A, idx_A, op_B, info_B, idx_B, op_C, info_C, idx_C, op_D, info_D, idx_D, prec);
-
-    TAPP_executor exec;
-    TAPP_status status;
-
-    float alpha = 2;
-
-    float A[36] = {
-        1,  2,  1.01, -1,
-        1,  2,  1.01, -1,
-        1,  2,  1.01, -1,
-
-        1,  2,  1.01, -1,
-        1,  2,  1.01, -1,
-        1,  2,  1.01, -1,
-
-        1,  2,  1.01, -1,
-        1,  2,  1.01, -1,
-        1,  2,  1.01, -1
-    };
-
-    float B[36] = {
-        1,  1,  1,
-        2,  2,  2,
-
-        3,  3,  3,
-        6,  6,  6,
-
-
-        1,  1,  1,
-        2,  2,  2,
-
-        3,  3,  3,
-        6,  6,  6,
-
-
-        1,  1,  1,
-        2,  2,  2,
-
-        3,  3,  3,
-        6,  6,  6
-    };
-
-    float beta = 0.5;
-
-    float C[16] = {
-        2,  4,  6,  8,
-        2,  4,  6,  8,
-
-        2,  4,  6,  8,
-        2,  4,  6,  8
-    };
-
-    float D[32] = {
-         1,  2,  3,  4,
-         5,  6,  7,  8,
-         9, 10, 11, 12,
-        13, 14, 15, 16,
-        
-         1,  2,  3,  4,
-         5,  6,  7,  8,
-         9, 10, 11, 12,
-        13, 14, 15, 16
-    };
-
-    TAPP_execute_product(plan, exec, &status, (void*)&alpha, (void*)A, (void*)B, (void*)&beta, (void*)C, (void*)D);
-
-    int64_t super_extents_D[3] = {4, 4, 2};
-    int64_t super_strides_D[3] = {1, 4, 16};
-
-    print_tensor_s(nmode_D, super_extents_D, super_strides_D, D);
 
     TAPP_destory_tensor_product(plan);
     TAPP_destory_tensor_info(info_A);
@@ -468,9 +350,9 @@ void conjugate() {
     TAPP_handle handle;
     TAPP_tensor_product plan;
     TAPP_element_op op_A = TAPP_IDENTITY;
-    TAPP_element_op op_B = TAPP_IDENTITY;
-    TAPP_element_op op_C = TAPP_IDENTITY;
-    TAPP_element_op op_D = TAPP_CONJUGATE;
+    TAPP_element_op op_B = TAPP_CONJUGATE;
+    TAPP_element_op op_C = TAPP_CONJUGATE;
+    TAPP_element_op op_D = TAPP_IDENTITY;
     int64_t idx_A[2] = {'a', 'b'};
     int64_t idx_B[2] = {'b', 'c'};
     int64_t idx_C[2] = {'a', 'c'};
