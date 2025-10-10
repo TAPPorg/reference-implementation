76b6953ba2606385dbbb4f54b554bb22c68a4397
Niklas Hörnblad <niklas_hornblad@hotmail.com>
2025-10-10 18:12:31 +0200

    Updated bindings allowing for non-contigous output tensor.

--- cutensor_bindings/cutensor_bind.h
+++ cutensor_bindings/cutensor_bind.h
@@ -7,6 +7,7 @@
 
 #include <unordered_map>
 #include <vector>
+#include <complex>
 
 #include "../src/tapp.h"
 
@@ -29,11 +30,11 @@ cutensorComputeDescriptor_t translate_prectype(TAPP_prectype prec);
 
 cutensorOperator_t translate_operator(TAPP_element_op op);
 
-//TAPP_handle create_TAPP_handle();
-
 TAPP_EXPORT TAPP_error create_handle(TAPP_handle* handle);
 
 TAPP_EXPORT TAPP_error create_executor(TAPP_executor* exec);
+
+size_t sizeof_datatype(TAPP_datatype type);
 
 typedef struct 
 {
@@ -41,15 +42,27 @@ typedef struct
     int64_t *extents;
     int64_t *strides;
     size_t elements;
-    size_t size;
+    size_t copy_size;
+    int64_t data_offset;
+    TAPP_datatype type;
     cutensorTensorDescriptor_t* desc;
 } cutensor_info;
 
 typedef struct 
 {
-    size_t sizeA;
-    size_t sizeB;
-    size_t sizeC;
-    size_t sizeD;
+    int64_t data_offset_A;
+    size_t copy_size_A;
+    int64_t data_offset_B;
+    size_t copy_size_B;
+    int64_t data_offset_C;
+    size_t copy_size_C;
+    int64_t data_offset_D;
+    size_t copy_size_D;
+    int64_t sections_D;
+    int64_t section_size_D;
+    int64_t sections_nmode_D;
+    int64_t* section_extents_D;
+    int64_t* section_strides_D;
+    TAPP_datatype type_D;
     cutensorPlan_t* plan;
 } cutensor_plan;
