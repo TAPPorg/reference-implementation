cb0200911483aced8bd986c22073bac64d792146
Juraj Hasik <juraj.hasik.sk@gmail.com>
2026-02-10 16:25:52 +0100

    make permutation path in cutensor optional, fix cmake, fix setting use_device_memory and demo-dynamic, test-dynamic

--- cutensor_bindings/src/attributes.cu
+++ cutensor_bindings/src/attributes.cu
@@ -6,7 +6,7 @@ TAPP_error TAPP_attr_set(TAPP_attr attr, TAPP_key key, void* value)
     switch (key)
     {
     case 0:
-        memcpy(value, (void*)handle_struct->attributes[0], sizeof(bool));
+        memcpy((void*)handle_struct->attributes[0], value, sizeof(bool));
         break;
     
     default:
@@ -15,13 +15,13 @@ TAPP_error TAPP_attr_set(TAPP_attr attr, TAPP_key key, void* value)
     return 0;
 }
 
-TAPP_error TAPP_attr_get(TAPP_attr attr, TAPP_key key, void** value)
+TAPP_error TAPP_attr_get(TAPP_attr attr, TAPP_key key, void* value)
 {
     struct handle* handle_struct = (struct handle*) attr;
     switch (key)
     {
     case 0:
-        memcpy((void*)handle_struct->attributes[0], value, sizeof(bool));
+        memcpy(value, (void*)handle_struct->attributes[0], sizeof(bool));
         break;
     
     default:
