620e5ce9cdd6d36f6e9322c87f2e80761c426a57
janbrandejs <honza.brandejs1@gmail.com>
2026-06-12 16:09:24 +0200

    fix consume smoke test to print TAPP_error.type/.code after struct migration

--- test/consume/smoke.c
+++ test/consume/smoke.c
@@ -6,12 +6,12 @@ int main(void) {
     TAPP_handle h;
     TAPP_error err = TAPP_create_handle(&h);
     if (!TAPP_check_success(err)) {
-        fprintf(stderr, "TAPP_create_handle failed: %d\n", (int)err);
+        fprintf(stderr, "TAPP_create_handle failed: type %d code %d\n", err.type, err.code);
         return EXIT_FAILURE;
     }
     err = TAPP_destroy_handle(h);
     if (!TAPP_check_success(err)) {
-        fprintf(stderr, "TAPP_destroy_handle failed: %d\n", (int)err);
+        fprintf(stderr, "TAPP_destroy_handle failed: type %d code %d\n", err.type, err.code);
         return EXIT_FAILURE;
     }
     printf("tapp-reference smoke test passed\n");
